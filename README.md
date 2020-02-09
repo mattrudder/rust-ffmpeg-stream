aff61c39fc8071b314db984a4c9e6b8448453d1d
Matt Rudder <matt@mattrudder.com>
2020-02-08 21:38:25 -0600

    Readme corrections; added DLL copy step to build script

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
+# FFmpeg with Rust
+
 ## Building FFmpeg for Windows
+
+If you'd like the skip this step, I have a version pre-compiled for VS2019 [available here](https://mattrudder.com/libs/vs2019/FFmpeg-4.2.1.zip).
+
 1. Install [msys2](https://www.msys2.org/)
+    - Optional: Update `msys2_shell.cmd` to include the settings for native symlinks and inheriting PATH. Both are in the file already, but commented out by default:
+        - `set MSYS=winsymlinks:nativestrict`
+        - `set MSYS2_PATH_TYPE=inherit`
 2. Launch a session with VS2019 Command Prompt settings
     - To start msys2 under Windows Terminal, use the following command: `C:\\msys64\\msys2_shell.cmd -defterm -no-start -mingw64`
-    - You can generate a shell script with the appropriate env variables with envdiff `cargo install envdiff`
+    - You can generate a shell script with the appropriate env variables with envdiff `cargo install --git=https://gitlab.com/antekone/envdiff.git`
         - Launch `envdiff` under a clean `cmd` session
         - Launch `envdiff` again under VS2019 command prompt and the output will be your shell script
 3. `mv /usr/bin/link.exe /usr/bin/link.exe.bak`
@@ -11,4 +19,3 @@
 6. Extract FFmpeg source to a working directory
 7. Configure: `./configure --target-os=win64 --arch=x86_64 --enable-x86asm --toolchain=msvc --enable-shared`
 8. Build and Install: `make && make install`
-
--- build.rs
+++ build.rs
@@ -1,8 +1,34 @@
+use std::path::{Path, PathBuf};
+use std::{fs, env};
+use std::ffi::OsStr;
+
 // TODO: Make this configurable;
 // for now, download from https://mattrudder.com/libs/vs2019/FFmpeg-4.2.1.zip
 // and extract to C:/dev/lib/FFmpeg-4.2.1
-const FFMPEG_PATH: &'static str = "C:\\dev\\lib\\FFmpeg-4.2.1";
+const FFMPEG_PATH: &'static str = "Z:\\dev\\lib\\FFmpeg-4.2.1";
 fn main() {
-    println!("cargo:rustc-env=CFLAGS=/I{}\\include", FFMPEG_PATH);
-    println!("cargo:rustc-link-search={}\\bin", FFMPEG_PATH);
+    let target = env::var("TARGET").unwrap();
+    if target.contains("pc-windows") {
+        println!("cargo:rustc-link-search={}\\bin", FFMPEG_PATH);
+        println!("cargo:rustc-env=CFLAGS=/I{}\\include", FFMPEG_PATH);
+
+        let mut manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        manifest_dir.push("target");
+        manifest_dir.push(env::var("PROFILE").unwrap());
+        
+        for entry in fs::read_dir(Path::new(FFMPEG_PATH).join("bin")).unwrap() {
+            if let Ok(x) = entry {
+                if let Some("dll") = x.path().extension().and_then(OsStr::to_str) {
+                    copy(&manifest_dir, &x.path());
+                }
+            }
+        }
+    }
+}
+
+
+fn copy<S: AsRef<std::ffi::OsStr> + ?Sized, P: Copy + AsRef<Path>>(target_dir_path: &S, file_name: P) {
+    if let Some(base_name) = file_name.as_ref().file_name() {
+        fs::copy(file_name, Path::new(&target_dir_path).join(base_name)).unwrap();
+    }
 }
